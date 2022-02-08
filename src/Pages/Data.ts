c77b38bbdafdedad07ab6ddadd37c70caae17a8e
Vasil Bit <vasil.bituni@gmail.com>
2022-02-08 17:37:55 +0100

    Add skills icons

--- src/Pages/Data.ts
+++ src/Pages/Data.ts
@@ -2,9 +2,26 @@ import img1 from '../assets/images/project1.png';
 import vaso from '../assets/images/vasil.png';
 import img2 from '../assets/images/project2.png';
 import img3 from '../assets/images/project3.png';
-import JSIcon from '../assets/images/JSicon.svg';
-import ReactIcon from '../assets/images/react.svg';
-import TSIcon from '../assets/images/typescript.svg';
+import HTMLIcon from '../assets/icons/html.svg';
+import CSSIcon from '../assets/icons/css.svg';
+import JSIcon from '../assets/icons/javascript.svg';
+import ReactIcon from '../assets/icons/react.svg';
+import TSIcon from '../assets/icons/typescript.svg';
+import SAASicon from '../assets/icons/saas.svg';
+import MaterialUI from '../assets/icons/material-ui.svg';
+import StyledComponentsIcon from '../assets/icons/styledcomponents.svg';
+import Git from '../assets/icons/git.svg';
+import Github from '../assets/icons/github.svg';
+import Node from '../assets/icons/node.svg';
+import MongoDB from '../assets/icons/mongodb.svg';
+import Linux from '../assets/icons/linux.svg';
+import Firebase from '../assets/icons/firebase.svg';
+import Netlify from '../assets/icons/netlify.svg';
+import Wordpress from '../assets/icons/wordpress.svg';
+import Webpack from '../assets/icons/webpack.svg';
+import Figma from '../assets/icons/figma.svg';
+import Illustrator from '../assets/icons/illustrator.svg';
+import Photoshop from '../assets/icons/photoshop.svg';
 
 export const homeObjOne = {
   imgStart: false,
@@ -36,7 +53,7 @@ export const homeObjThree = {
   imgStart: false,
   headline: 'Find me if you can',
   description:
-    'A simple game where you have to search for the listed characters in the image. The purpose of creating this game was to practice my knowledge of Firebase, a NoSQL database.',
+    "A simple game where you have to search for the listed characters in the image. This project uses Firestore's NoSQL database to store the coordinates of the characters as well as the list of winners.",
   subtittle: 'Firebase Sass react typescript',
   img: img3,
   alt: 'hello',
@@ -58,17 +75,24 @@ export const homeObjAbout = {
 };
 
 export const homeObjSkills = [
+  { img: HTMLIcon, tittle: 'HTML' },
+  { img: CSSIcon, tittle: 'CSS' },
   { img: JSIcon, tittle: 'JavaScript' },
-  {
-    img: TSIcon,
-    tittle: 'TypeScript',
-  },
-  { img: ReactIcon, tittle: 'GitHub' },
-  { img: ReactIcon, tittle: 'Jest' },
-  { img: ReactIcon, tittle: 'HTML' },
-  { img: ReactIcon, tittle: 'SASS' },
-  { img: ReactIcon, tittle: 'Firgma' },
-  { img: ReactIcon, tittle: 'Photoshop' },
-  { img: ReactIcon, tittle: 'MaterialUI' },
-  { img: ReactIcon, tittle: 'Firebase' },
+  { img: ReactIcon, tittle: 'React' },
+  { img: TSIcon, tittle: 'TypeScript' },
+  { img: SAASicon, tittle: 'SASS' },
+  { img: MaterialUI, tittle: 'SASS' },
+  { img: StyledComponentsIcon, tittle: 'Styled Components' },
+  { img: Node, tittle: 'Node.js' },
+  { img: MongoDB, tittle: 'MongoDB' },
+  { img: Webpack, tittle: 'Webpack' },
+  { img: Firebase, tittle: 'Firebase' },
+  { img: Git, tittle: 'Git' },
+  { img: Linux, tittle: 'Linux' },
+  { img: Github, tittle: 'Github' },
+  { img: Netlify, tittle: 'Netlify' },
+  { img: Wordpress, tittle: 'Wordpress' },
+  { img: Figma, tittle: 'Figma' },
+  { img: Illustrator, tittle: 'Illustrator' },
+  { img: Photoshop, tittle: 'Photoshop' },
 ];
